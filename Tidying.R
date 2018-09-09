9895df7e9a3613e619efbd3d08626795dcc88525
Lex Lai <lex.lai90@gmail.com>
2018-09-09 17:35:08 -0400

    Tidying continued

--- Tidying.R
+++ Tidying.R
@@ -1,23 +1,31 @@
-library("tidyverse")
-View(refine_original)
+library(tidyverse)
 library(magrittr)
 library(dplyr)
 library(tidyr)
 library(stringr)
 
+##Loading the Data
+library(readxl)
+refine_original <- read_excel("C:/Users/lexla/Desktop/refine_original.csv")
+View(refine_original)
+
 #Company Names
-refine_original$company <- refine_original$company %>% tolower() %>% gsub(pattern = "0", replacement = "o") %>% gsub(pattern = "f", replacement = "ph") %>% arrange(company)
-refine_original$company <- sub("a.*o", replacement = "akzo", refine_original$company)
-refine_original$company <- sub("p.*s", replacement = "phillips", refine_original$company)
+refine_original <- 
+  refine_original %>% mutate(company = tolower(company)) %>%
+  mutate(company = str_replace(company,"a.*o", "akzo")) %>% 
+  mutate(company = str_replace(company, "0", "o")) %>% 
+  mutate(company = str_replace(company, "p.*s", "phillips")) %>% 
+  mutate(company = str_replace(company, "f", "ph")) %>%
+  mutate(company = str_replace(company,"u.*r", "unilever"))
 
 #Product Code/Number
-refine_original <- separate(refine_original, `Product code / number`, into = c("Product Code", "Number"), sep = "-")
+refine_original <- 
+  separate(refine_original, `Product code / number`, into = c("Product Code", "Number"), sep = "-")
 refine_original$`Product Code` <- sub("p", replacement = "Smartphone", refine_original$`Product Code` )
 refine_original$`Product Code` <- sub("v", replacement = "TV", refine_original$`Product Code` )
 refine_original$`Product Code` <- sub("x", replacement = "Laptop", refine_original$`Product Code` )
 refine_original$`Product Code` <- sub("q", replacement = "Tablet", refine_original$`Product Code` )
 
-refine_original
 
 
 
