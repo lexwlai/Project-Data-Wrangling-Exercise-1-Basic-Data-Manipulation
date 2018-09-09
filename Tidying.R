27029b005dc7be91d7a18c1a2d04ad4e034eb5f9
Lex Lai <lex.lai90@gmail.com>
2018-09-09 17:49:47 -0400

    9/10/18

--- Tidying.R
+++ Tidying.R
@@ -9,29 +9,34 @@ library(readxl)
 refine_original <- read_excel("C:/Users/lexla/Desktop/refine_original.csv")
 View(refine_original)
 
+refine <- tbl_df(refine_original)
+
+
 #Company Names
-refine_original <- 
-  refine_original %>% mutate(company = tolower(company)) %>%
+refine <-
+  refine %>%
+  mutate(company = tolower(company)) %>%
   mutate(company = str_replace(company,"a.*o", "akzo")) %>% 
   mutate(company = str_replace(company, "0", "o")) %>% 
   mutate(company = str_replace(company, "p.*s", "phillips")) %>% 
   mutate(company = str_replace(company, "f", "ph")) %>%
   mutate(company = str_replace(company,"u.*r", "unilever"))
 
+
 #Product Code/Number
-refine_original <- 
+refine <- 
   separate(refine_original, `Product code / number`, into = c("Product Code", "Number"), sep = "-")
-refine_original$`Product Code` <- sub("p", replacement = "Smartphone", refine_original$`Product Code` )
-refine_original$`Product Code` <- sub("v", replacement = "TV", refine_original$`Product Code` )
-refine_original$`Product Code` <- sub("x", replacement = "Laptop", refine_original$`Product Code` )
-refine_original$`Product Code` <- sub("q", replacement = "Tablet", refine_original$`Product Code` )
+refine$`Product Code` <- sub("p", replacement = "Smartphone", refine_original$`Product Code` )
+refine$`Product Code` <- sub("v", replacement = "TV", refine_original$`Product Code` )
+refine$`Product Code` <- sub("x", replacement = "Laptop", refine_original$`Product Code` )
+refine$`Product Code` <- sub("q", replacement = "Tablet", refine_original$`Product Code` )
+
+#Address/Geocoding
+refine <-
+  unite(refine, full_address, address, city, country, sep = ", ")
 
 
-
-
-
-
-
+View(refine)
 
 
 
