c0f3b5d0be0b87eab48d3ee3df409c5d72fc9450
Lex Lai <lex.lai90@gmail.com>
2018-09-11 21:41:19 -0400

    9/11/18

--- Tidying.R
+++ Tidying.R
@@ -18,10 +18,11 @@ refine <-
   mutate(company = tolower(company)) %>%
   mutate(company = str_replace(company,"a.*o", "akzo")) %>% 
   mutate(company = str_replace(company, "0", "o")) %>% 
-  mutate(company = str_replace(company, "p.*s", "phillips")) %>% 
   mutate(company = str_replace(company, "f", "ph")) %>%
-  mutate(company = str_replace(company,"u.*r", "unilever"))
-
+  mutate(company = str_replace(company, "ph.*s", "philips")) %>% 
+  mutate(company = str_replace(company,"u.*r", "unilever")) %>% 
+  mutate(company = str_replace(company, "v.*n", "van houten")) 
+  
 #Product Code/Number
 refine <- 
   separate(refine, `Product code / number`, into = c("Product Code", "Number"), sep = "-")
@@ -34,6 +35,29 @@ refine$`Product Code` <- sub("q", replacement = "Tablet", refine$`Product Code` 
 refine <-
   unite(refine, full_address, address, city, country, sep = ", ")
 
+##Dummy Variables
+#Company
+refine <-
+  refine %>%
+  mutate(company_philips = 
+           ifelse(refine$company == "philips", "1", "0")) %>%
+  mutate(company_akzo = 
+           ifelse(refine$company == "akzo", "1", "0")) %>%
+  mutate(company_van_houten = 
+           ifelse(refine$company == "van houten", "1", "0")) %>%
+  mutate(company_unilever = 
+           ifelse(refine$company == "unilever", "1", "0"))
+#Products
+refine <-
+  refine %>%
+  mutate(product_smartphone = 
+           ifelse(refine$`Product Code` == "Smartphone", "1", "0")) %>%
+  mutate(product_tv = 
+           ifelse(refine$`Product Code` == "TV", "1", "0")) %>%
+  mutate(product_laptop = 
+           ifelse(refine$`Product Code` == "Laptop", "1", "0")) %>%
+  mutate(company_tablet = 
+           ifelse(refine$`Product Code` == "Tablet", "1", "0"))
 
 View(refine)
 
